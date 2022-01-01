7898f7aa7a83098a903ef4812ddf163bcd1c1eff
colin-mcl <74631009+colin-mcl@users.noreply.github.com>
2022-01-01 15:17:46 -0500

    Created puzzle class (#18)
    Created and linked empty header and .cpp file for puzzle class
    
    Resolves #14

---
+++ Puzzle.cpp
@@ -0,0 +1,9 @@
+/* Puzzle.cpp
+ * By Colin Mclaughlin
+ * January 2022
+ *
+ *
+ * TODO: write documentation
+ */
+ 
+ #include "Puzzle.h"
--- board.h
+++ board.h
@@ -1,4 +1,4 @@
-/* board.h
+/* Board.h
  * By Colin Mclaughlin
  * December 2021
  *
