0d65d1bef93e072dcf39cc7bc277305fa2af859f
colin-mcl <colin.mclaughlin02@gmail.com>
2022-01-01 21:31:42 -0500

    Added bitpacking
    Added bitpacking integers to keep track of found values in rows, columns , and squares of the sudoku board.

--- Puzzle.cpp
+++ Puzzle.cpp
@@ -15,8 +15,65 @@
          cols[i] = 0;
          squares[i] = 0;
      }
+     
+    initVals();
  }
  
  void Puzzle::solve() {
+
      board_p->printBoard();
+     
+     for (int i = 0; i < 9; i++) {
+         for (int j = 1; j <= 9; j++) {
+             if (numFound(squares[i], j))
+                std::cout << j << " in square " << i << "\n";
+         }
+     }
+ }
+ 
+ void Puzzle::initVals() {
+     for (int i = 0; i < 9; i++) {
+         for (int j = 0; j < 9; j++) {
+             short val = board_p->get(i, j);
+             
+             if (val != 0) {
+                 //set num found in the row, column, and square
+                 rows[i] = setFound(rows[i], val);
+                 cols[j] = setFound(cols[j], val);
+                 
+                 int squarePos = ((i / 3) * 3) + (j / 3);
+                 squares[squarePos] = setFound(squares[squarePos], val);
+             }
+             
+         }
+     }
+ }
+ 
+ int Puzzle::setFound(int found, short val) {
+     int mask = 1;
+     mask = mask << (val - 1);
+     
+     found = found | mask;
+     return found;
+ }
+ 
+ //returns true if the value can be placed at position (row, col) in the board
+ bool Puzzle::canPlace(short val, int row, int col) {
+     int squarePos = ((row / 3) * 3) + (col / 3);
+     
+     //if the number is not found in the row, column and square, return true
+     return not (numFound(rows[row], val) && numFound(cols[col], val) &&
+            numFound(squares[squarePos], val));
+ }
+ 
+ bool Puzzle::numFound(int found, short val) {
+     int mask = 1;
+     mask = mask << (val - 1);
+     
+     mask = mask & found;
+     
+     if (mask == 0)
+        return false;
+    else
+        return true;
  }
--- driver.cpp
+++ driver.cpp
@@ -5,9 +5,12 @@
 
 int main(int argc, char *argv[]) {
     
-    Board board;
+    std::ifstream input(argv[1]);
+    
+    Board board(input);
     Puzzle puzzle(&board);
     puzzle.solve();
     
+    input.close();
     return 0;
 }
