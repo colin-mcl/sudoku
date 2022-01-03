7a08990d53fb3fdf3be7f814153ea9a4420c88e0
colin-mcl <74631009+colin-mcl@users.noreply.github.com>
2022-01-02 19:51:47 -0500

    Added command line messages (#24)
    Resolves #22

--- driver.cpp
+++ driver.cpp
@@ -3,14 +3,42 @@
 #include "Board.h"
 #include "Puzzle.h"
 
+void usage_error();
+void invalid_file(std::string filename);
+void run(std::istream &input);
+
 int main(int argc, char *argv[]) {
     
-    std::ifstream input(argv[1]);
+    if (argc > 2) 
+        usage_error();
     
+    
+    if (argc == 1)
+        run(std::cin);
+    else {
+        std::ifstream infile(argv[1]);
+        if (infile.fail())
+            invalid_file(argv[1]);
+        
+        run(infile);
+        infile.close();
+    }
+    
+    return 0;
+}
+
+void usage_error() {
+    std::cerr << "Usage error: ./sudoku [filename]\n";
+    exit(EXIT_FAILURE);
+}
+
+void invalid_file(std::string filename) {
+    std::cerr << "Failed to open " << filename << "\n";
+    exit(EXIT_FAILURE);
+}
+
+void run(std::istream &input) {
     Board board(input);
     Puzzle puzzle(&board);
     puzzle.solve();
-    
-    input.close();
-    return 0;
 }
